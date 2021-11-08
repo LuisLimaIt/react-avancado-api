8808caee539b4a0c1760ae7bf4c8c6866a012ea0
Luis Paulo de Lima Soares <pls.luis@gmail.com>
2021-11-08 16:06:09 -0300

    Correção na configuração do netlify webhook

--- api/landing-page/models/landing-page.js
+++ api/landing-page/models/landing-page.js
@@ -1,19 +1,21 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-const axios = require('axios');
-const netlifyWebhook = strapi.config.get("custom.netlifywebhook")
-module.exports = {
-  lifecycles: {
-    async afterCreate(_, __) {
-      netlifyWebhook && axios.post(netlifywebhook)
-    },
-    async afterUpdate(_, __, ___) {
-      netlifyWebhook && axios.post(netlifywebhook)
-    },
-  },
-};
+ module.exports = {};
+ const axios = require("axios");
+ const netlifyWebhook = strapi.config.get("custom.netlifyWebhook");
+
+ module.exports = {
+   lifecycles: {
+     async afterCreate(_, __) {
+       netlifyWebhook && axios.post(netlifyWebhook);
+     },
+     async afterUpdate(_, __, ___) {
+       netlifyWebhook && axios.post(netlifyWebhook);
+     },
+   },
+ };
